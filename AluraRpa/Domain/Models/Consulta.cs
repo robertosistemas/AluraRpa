98fdebc5e2a8f9a9b5143ad71bd46353e685bb03
Roberto Carlos <robertosistemas@hotmail.com>
2024-06-03 11:18:38 -0300

    Arquivos relacionados ao domínio da aplicação

---
+++ AluraRpa/Domain/Models/Consulta.cs
@@ -0,0 +1,11 @@
+﻿namespace AluraRpa.Domain.Models
+{
+    public class Consulta
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Professores { get; set; } = string.Empty;
+        public string CargaHoraria { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+    }
+}
---
+++ AluraRpa/Domain/Validators/ConsultaValidator.cs
@@ -0,0 +1,16 @@
+﻿using AluraRpa.Domain.Models;
+using FluentValidation;
+
+namespace AluraRpa.Domain.Validators
+{
+    public class ConsultaValidator : AbstractValidator<Consulta>
+    {
+        public ConsultaValidator()
+        {
+            RuleFor(consulta => consulta.Titulo).NotEmpty();
+            RuleFor(consulta => consulta.Professores).NotEmpty();
+            RuleFor(consulta => consulta.CargaHoraria).NotEmpty();
+            RuleFor(consulta => consulta.Descricao).NotEmpty();
+        }
+    }
+}
