3b2ca9c3695c25c409e47e6db3ed9fbc965818bf
Roberto Carlos <robertosistemas@hotmail.com>
2024-06-03 11:18:14 -0300

    Arquivos relacionados a criação e versionamento das migrações do banco de dados

---
+++ AluraRpa/Migrations/20240603054600_Init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AluraRpa.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Consulta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Professores = table.Column<string>(type: "nvarchar(max)", maxLength: 4096, nullable: false),
+                    CargaHoraria = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", maxLength: 4096, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consulta", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consulta");
+        }
+    }
+}
